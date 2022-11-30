bbb03f53990f3ff88aa4661647046cb0ad4fd148
Jair <jairaceves56@gmail.com>
2022-11-30 08:52:32 -0600

    Agregamos una funcion para cuando cargue la informacion

--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -1,7 +1,18 @@
 <template>
   <Navbar />
 
-  <div class="d-flex">
+  <div v-if="isLoading"
+    class="row justify-content-center">
+    <div class="col-3 alert-info text-center mt-5">
+      Espere Por Favor
+      <h3 class="mt-2">
+        <i class="fa fa-spin fa-sync"></i>
+      </h3>
+    </div>
+  </div>
+
+  <div v-else
+    class="d-flex">
     <div class="col-3">
       <EntryList />
     </div>
@@ -14,7 +25,7 @@
 
 <script>
 import { defineAsyncComponent } from 'vue';
-import { mapActions } from 'vuex';
+import { mapActions, mapState } from 'vuex';
 
 export default {
   components: {
@@ -23,7 +34,11 @@ export default {
   },
 
   methods: {
-    ...mapActions('journal', ['loadEntries'])
+    ...mapActions('journal', ['loadEntries']),
+  },
+
+  computed: {
+    ...mapState('journal', ['isLoading'])
   },
 
   created() {
