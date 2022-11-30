5edabc9d58533393c475d029d7a5314be20d1836
Jair <jairaceves56@gmail.com>
2022-11-29 18:22:20 -0600

    Entradas por defecto en el State de la App

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,5 +1,24 @@
 
 export default () => ({
     isLoading: true,
-    entries: [1,2,3,4,5,6]
+    entries: [
+        {
+            id: new Date().getTime(), // Fecha Actual 
+            date: new Date().toDateString(),  // Sat 23 Jully
+            text: 'Aute elit proident nostrud nisi reprehenderit reprehenderit nostrud aliqua proident laborum enim occaecat commodo dolore. Ex anim et laborum voluptate officia incididunt consequat est minim dolore duis excepteur. Exercitation mollit deserunt adipisicing sint minim ad amet in anim aliquip adipisicing. Sint et eu excepteur qui sint laborum consequat id officia amet.',
+            picture: null, // https://
+        },
+        {
+            id: new Date().getTime() + 1000, // Fecha Actual 
+            date: new Date().toDateString(),  // Sat 23 Jully
+            text: 'Esse aliqua eiusmod non occaecat est est culpa dolor duis ipsum nostrud consequat adipisicing occaecat. Voluptate esse reprehenderit amet deserunt id ullamco magna labore occaecat. Ea aute enim ea qui esse laboris amet aliqua amet exercitation enim ipsum ea commodo.',
+            picture: null, // https://
+        },
+        {
+            id: new Date().getTime() + 2000, // Fecha Actual 
+            date: new Date().toDateString(),  // Sat 23 Jully
+            text: 'Magna est laborum sunt voluptate mollit magna reprehenderit sit esse commodo est laboris. Voluptate nostrud exercitation pariatur fugiat. Cillum reprehenderit ipsum enim aliqua eiusmod in veniam. Ad irure ex amet sint aute sunt. In aliquip quis enim dolor non Lorem est ea cupidatat labore ipsum commodo incididunt id. Nulla ut esse Lorem minim excepteur sint irure aliqua non ea aliquip non Lorem. Duis ullamco officia adipisicing incididunt sunt duis et in elit sit est dolore.',
+            picture: null, // https://
+        },
+    ]
 })
